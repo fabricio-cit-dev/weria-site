941eb0c68b6b77121cf12c0ee82746604952ad61
fabricio-cit-dev <fabricio.feo@citechnology.com.br>
2023-07-17 18:33:48 -0300

    Mudanças no forms de contato, porém faltando ajustes

--- src/components/Contact/ContactOneForm.jsx
+++ src/components/Contact/ContactOneForm.jsx
@@ -1,33 +1,80 @@
 import React from "react";
-import ContactImage from "../../assets/img/images/WAVY_B_1-removebg-preview.png";
+import ContactImage from "../../assets/img/images/chat-image-removebg.png";
+import NameIcon from "../../assets/img/icon/icons8-nome-24.png";
+import EmailIcon from "../../assets/img/icon/icons8-nova-mensagem-24.png";
 
 const ContactOneForm = () => {
   return (
     <div className="contact-form-wrap">
+      <div className="leftDiv">
+        <div className="DivImageContact">
+          <img className="ImageContact" src={ContactImage} />
+        </div>
+        <div className="step-map">
+          <div className="step">
+            <span className="step-number">1</span>
+            <span className="step-title">
+              Receba uma proposta personalizada rapidamente
+            </span>
+          </div>
+          <div className="step">
+            <span className="step-number">2</span>
+            <span className="step-title">
+              Converse conosco para discutir suas necessidades
+            </span>
+          </div>
+          <div className="step">
+            <span className="step-number">3</span>
+            <span className="step-title">
+              Iniciaremos a construção do seu projeto juntos
+            </span>
+          </div>
+        </div>
+      </div>
       <form action="#" className="DivForms">
-        <h2 style={{display: "flex", justifyContent: "center", padding: "1rem", color: "yellow"}}>
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "1rem",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.25)",
+          }}
+        >
           Fale Conosco
         </h2>
         <div className="row">
           <div className="col-md-6">
             <div className="form-grp">
-              {/* <label htmlFor="nome" className="forms-title">
-                Nome Completo
-              </label> */}
-              <input
-                type="text"
-                id="nome"
-                placeholder="Nome Completo"
-                required
-              />
+              <div className="input-with-icon">
+                <img
+                  src={NameIcon}
+                  alt="Ícone de nome"
+                  className="input-icon"
+                />
+                <input
+                  type="text"
+                  id="nome"
+                  placeholder="Digite seu nome"
+                  required
+                />
+              </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="form-grp">
-              {/* <label htmlFor="email" className="forms-title">
-                Email
-              </label> */}
-              <input type="email" id="email" placeholder="Email" required />
+              <div className="input-with-icon">
+                <img
+                  src={EmailIcon}
+                  alt="Ícone de e-mail"
+                  className="input-icon"
+                />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Digite seu email"
+                  required
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -58,9 +105,6 @@ const ContactOneForm = () => {
           </button>
         </div>
       </form>
-      <div className="DivImageContact">
-        <img src={ContactImage} className="ImageContact" />
-      </div>
     </div>
   );
 };
